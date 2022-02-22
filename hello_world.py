e9d4c2df563ad72a97bf199a4805c50d830a377b
Miguel Santacruz <miguel.santacruz@mths.ca>
2022-02-22 04:23:30 +0000

    hello_world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Miguel Santacruz
+# Created on: February 2022
+# This is the "Hello, world!" program, with proper style
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
